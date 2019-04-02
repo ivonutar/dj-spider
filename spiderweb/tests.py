5770f52ada25b36be6819e8b50e1aab467a45d30
Ivo Nutar <ivo.nutar@gmail.com>
2019-04-02 21:04:06 +0200

    Add tests

--- spiderweb/tests.py
+++ spiderweb/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Target
+from .utils import get_links
+import responses
 
-# Create your tests here.
+
+class TargetTest(TestCase):
+
+    test_url = 'http://test.nonexistent'
+
+    def setUp(self) -> None:
+
+        self.test_target = Target.objects.create(target_url=self.test_url)
+
+    def test_create_target(self):
+        self.assertEqual(self.test_target.target_url, self.test_url)
+
+
+class TestUtils(TestCase):
+
+    test_url = 'http://test.nonexistent'
+
+    @responses.activate
+    def test_get_links(self):
+        responses.add(responses.GET, self.test_url,
+                      body='<a href="test_links"></a>', status=200)
+        found_links = get_links(self.test_url)
+
+        self.assertEqual(found_links, ['test_links'])
--- spiderweb/viewsets.py
+++ spiderweb/viewsets.py
@@ -1,9 +1,7 @@
 from rest_framework import viewsets
 from .models import Target
 from . serializers import TargetSerializer
-import requests
-from bs4 import BeautifulSoup
-from django.http import HttpResponse
+from .utils import get_links
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -18,18 +16,8 @@ class TargetViewSet(viewsets.ModelViewSet):
 
         target_id = kwargs.get('pk')
         target_url = Target.objects.get(id=target_id).target_url
-        try:
-            r = requests.get('{}'.format(target_url))
-        except requests.exceptions.MissingSchema as ex:
-            return HttpResponse("Error: {}".format(ex))
-        html = r.text
-        soup = BeautifulSoup(html)
+
         links = list()
-        for link in soup.find_all('a'):
-            link_url = link.get('href')
-            if link_url in ['#']:
-                pass
-            else:
-                links.append(link_url)
-        links = sorted(links)
+        links.append(get_links(target_url))
+
         return Response({'paths': links})
